ab394422fe7b936e0f1d20156eacdd7b27fc16dd
Gittel Rosenblatt <gittel.rosenblatt1@gmail.com>
2026-06-21 02:20:42 -0400

    Add iris basic layout and buttons

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class IrisApp:
         self.root.geometry("1460x800")
         
         SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-        logo_path = os.path.join(SCRIPT_DIR, 'iris-logo.png')
+        logo_path = os.path.join(SCRIPT_DIR, 'iris-icon.png')
         img_original = Image.open(logo_path).convert("RGBA")
         
         canvas_size = (256, 256)
@@ -34,7 +34,29 @@ class IrisApp:
         self.create_main_dashboard()
         
     def create_main_dashboard(self):
-        pass
+        self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(1, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.root.rowconfigure(1, weight=1)
+        self.root.rowconfigure(2, weight=1)
+
+        self.time_btn = tk.Button(self.root, text="Time", font=("Atkinson Hyperlegible", 36, "bold"))
+        self.time_btn.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+
+        self.weather_btn = tk.Button(self.root, text="Weather", font=("Atkinson Hyperlegible", 36, "bold"))
+        self.weather_btn.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
+
+        self.todo_btn = tk.Button(self.root, text="To Do", font=("Atkinson Hyperlegible", 36, "bold"))
+        self.todo_btn.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
+
+        self.notes_btn = tk.Button(self.root, text="Notes", font=("Atkinson Hyperlegible", 36, "bold"))
+        self.notes_btn.grid(row=1, column=1, sticky="nsew", padx=10, pady=10)
+
+        self.email_btn = tk.Button(self.root, text="Email", font=("Atkinson Hyperlegible", 36, "bold"))
+        self.email_btn.grid(row=2, column=0, sticky="nsew", padx=10, pady=10)
+
+        self.forms_btn = tk.Button(self.root, text="Forms", font=("Atkinson Hyperlegible", 36, "bold"))
+        self.forms_btn.grid(row=2, column=1, sticky="nsew", padx=10, pady=10)
 
 if __name__ == "__main__":
     root = tk.Tk()
