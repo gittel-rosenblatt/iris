0516b9dddd8e210cf423a21d68056a9d467c4f27
Gittel Rosenblatt <gittel.rosenblatt1@gmail.com>
2026-06-28 20:52:43 -0400

    Make HTML and CSS for our story and legal pages

--- app.py
+++ app.py
@@ -6,5 +6,33 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/our-story')
+def story():
+    return render_template('story.html') 
+    
+@app.route('/terms')
+def terms():
+    return render_template('terms.html') 
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html') 
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html') 
+    
+@app.route('/workspace')
+def forms():
+    return render_template('workspace.html') 
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html') 
+
 if __name__ == "__main__":
     app.run(debug=True)
