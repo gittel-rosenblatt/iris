c8f24facb15bf90179047a6e0957de457feb4702
Gittel Rosenblatt <gittel.rosenblatt1@gmail.com>
2026-07-07 01:08:04 -0400

    design and style the signup section of the login page

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,12 +1,51 @@
-const passwordInput = document.querySelector('#password');
-const toggleButton = document.querySelector('#toggle-password');
+const loginInput = document.querySelector('#password');
+const loginToggle = document.querySelector('#toggle-login-pass');
 
-toggleButton.addEventListener('click', () => {
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        toggleButton.textContent = 'Hide'; 
+loginToggle.addEventListener('click', () => {
+    if (loginInput.type === 'password') {
+        loginInput.type = 'text';
+        loginToggle.textContent = 'Hide'; 
     } else {
-        passwordInput.type = 'password';
-        toggleButton.textContent = 'Show';
+        loginInput.type = 'password';
+        loginToggle.textContent = 'Show';
     }
+});
+
+const signupInput = document.querySelector('#choose-password');
+const signupToggle = document.querySelector('#toggle-signup-pass');
+
+signupToggle.addEventListener('click', () => {
+    if (signupInput.type === 'password') {
+        signupInput.type = 'text';
+        signupToggle.textContent = 'Hide'; 
+    } else {
+        signupInput.type = 'password';
+        signupToggle.textContent = 'Show';
+    }
+});
+
+const confirmInput = document.querySelector('#confirm-password');
+const confirmToggle = document.querySelector('#toggle-confirm-pass');
+
+confirmToggle.addEventListener('click', () => {
+    if (confirmInput.type === 'password') {
+        confirmInput.type = 'text';
+        confirmToggle.textContent = 'Hide'; 
+    } else {
+        confirmInput.type = 'password';
+        confirmToggle.textContent = 'Show';
+    }
+});
+
+
+const switchToSignup = document.getElementById('switch-to-signup');
+const switchToLogin = document.getElementById('switch-to-login');
+const body = document.body; 
+
+switchToSignup.addEventListener('click', () => {
+    body.classList.add('is-switched');
+});
+
+switchToLogin.addEventListener('click', () => {
+    body.classList.remove('is-switched');
 });
