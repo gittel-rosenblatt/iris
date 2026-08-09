d5116803a59541af581c0e71cea44e2153566c98
Gittel Rosenblatt <gittel.rosenblatt1@gmail.com>
2026-08-09 14:16:41 -0400

    Begin Flask routing for login and profile pages

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+FAKE_USERS_DB = {
+    'testuser': {
+        'password': '1234',
+        'question': 'pet',
+        'answer': 'fluffy'
+    }
+}
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -18,9 +26,43 @@ def terms():
 def privacy():
     return render_template('privacy.html') 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        print(f"Attempted login with: {username} and password: {password}")
+
+        if username == 'testuser' and password == '1234':
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('login.html')
+        # return redirect(url_for('dashboard'))
+    
     return render_template('login.html') 
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        question = request.form.get('security_question')
+        answer = request.form.get('security-answer')
+
+        if username in FAKE_USERS_DB:
+            print(f"Signup failed: Username '{username}' already exists!")
+            return render_template('login.html')
+
+        FAKE_USERS_DB[username] = {
+            'password': password,
+            'question': question,
+            'answer': answer
+        }
+
+        print(f"Successfully registered new user: {username}")
+
+        return redirect(url_for('profile'))
 
 @app.route("/forgot-password")
 def forgot_password():
@@ -30,21 +72,33 @@ def forgot_password():
 def reset_password():
     return render_template("reset-question.html") 
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['POST'])
 def dashboard():
     return render_template('dashboard.html') 
     
-@app.route('/workspace')
+@app.route('/workspace', methods=['POST'])
 def workspace():
     return render_template('workspace.html') 
 
-@app.route('/profile')
+@app.route('/profile', methods=['POST'])
 def profile():
     return render_template('profile.html') 
 
 @app.route("/contact-and-faqs")
 def contact():
     return render_template("contact.html") 
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('login'))
+
+@app.route('/update-profile', methods=['POST'])
+def update_profile():
+    return redirect(url_for('profile'))
+
+@app.route('/update-password', methods=['POST'])
+def update_password():
+    return redirect(url_for('profile'))
 
 if __name__ == "__main__":
     app.run(debug=True)
