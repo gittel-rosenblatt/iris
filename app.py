57157e2a256dc70172afea17b60eec83ffa17ecd
Gittel Rosenblatt <gittel.rosenblatt1@gmail.com>
2026-08-09 20:30:38 -0400

    Begin SQL setup

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
 FAKE_USERS_DB = {
     'testuser': {
         'password': '1234',
-        'question': 'pet',
+        'security_question': 'pet',
         'answer': 'fluffy'
     }
 }
 
+load_dotenv()
+
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///iris.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    security_question = db.Column(db.String(200), nullable=False)
+    security_answer = db.Column(db.String(120), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
@@ -34,11 +55,11 @@ def login():
 
         print(f"Attempted login with: {username} and password: {password}")
 
-        if username == 'testuser' and password == '1234':
+        if username in FAKE_USERS_DB and FAKE_USERS_DB[username]['password'] == password:
+            session['user'] = username
             return redirect(url_for('dashboard'))
         else:
             return render_template('login.html')
-        # return redirect(url_for('dashboard'))
     
     return render_template('login.html') 
 
@@ -56,32 +77,51 @@ def signup():
 
         FAKE_USERS_DB[username] = {
             'password': password,
-            'question': question,
-            'answer': answer
+            'security_question': question,
+            'security_answer': answer
         }
 
         print(f"Successfully registered new user: {username}")
 
+        session['user'] = username
         return redirect(url_for('profile'))
 
-@app.route("/forgot-password")
+@app.route("/forgot-password", methods=['GET', 'POST'])
 def forgot_password():
+    if request.method == 'POST':
+        username = request.form.get('username')
+
+        if username in FAKE_USERS_DB:
+            question = FAKE_USERS_DB[username].get('security_question')
+            return render_template('reset-question.html', username=username, security_question=question)
+        else:
+            return render_template('forgot-password.html', error=True)
+
     return render_template("forgot-password.html") 
 
 @app.route("/reset-password")
 def reset_password():
     return render_template("reset-question.html") 
 
-@app.route('/dashboard', methods=['POST'])
+@app.route('/dashboard')
 def dashboard():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+
     return render_template('dashboard.html') 
     
-@app.route('/workspace', methods=['POST'])
+@app.route('/workspace')
 def workspace():
+    if 'user' not in session:
+            return redirect(url_for('login'))
+    
     return render_template('workspace.html') 
 
-@app.route('/profile', methods=['POST'])
+@app.route('/profile')
 def profile():
+    if 'user' not in session:
+            return redirect(url_for('login'))
+    
     return render_template('profile.html') 
 
 @app.route("/contact-and-faqs")
@@ -90,6 +130,7 @@ def contact():
 
 @app.route('/logout')
 def logout():
+    session.pop('user', None)
     return redirect(url_for('login'))
 
 @app.route('/update-profile', methods=['POST'])
